4ab842bf1e0bab8a8a2a5c56b808d9bf2fc23bce
Vladislav Kolesnikov <forshortmrmeth@gmail.com>
2025-05-22 11:10:12 +0200

    feat: initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      new URL('https://images.chesscomfiles.com/uploads/v1/user/**'),
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
